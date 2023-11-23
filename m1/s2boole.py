fbf889c35323a343dfc176d40162ae59a99426e4
Emanuele <egalli64@gmail.com>
2023-11-23 20:02:12 +0100

    booleans

---
+++ m1/s2boole.py
@@ -0,0 +1,21 @@
+"""
+ Introduction to Software Development (Python)
+ 
+ https://github.com/egalli64/dev-intro
+
+ Boolean variables and operators
+"""
+# AND
+conjunction = True and False
+print("True and False is", conjunction)
+
+# OR
+inclusiveDisjunction = True or False
+print("True or False is", inclusiveDisjunction)
+
+# NOT
+print("Not true is", not True)
+
+# XOR
+exclusiveDisjunction = True ^ True
+print("True xor True is", exclusiveDisjunction)
