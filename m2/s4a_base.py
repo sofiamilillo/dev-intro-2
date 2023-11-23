dfb5773ded4e1ffd47d0e0a93924024ff227a730
Emanuele <egalli64@gmail.com>
2023-11-23 21:30:38 +0100

    low-level operations

---
+++ m2/s4a_base.py
@@ -0,0 +1,14 @@
+"""
+ Introduction to Software Development (Python)
+ 
+ https://github.com/egalli64/dev-intro
+
+ Some low-level instructions
+"""
+a = 7
+b = 3
+print("a is", a)
+print("b is", b)
+
+print("a + b is", a + b)
+print("a * b is", a * b)
