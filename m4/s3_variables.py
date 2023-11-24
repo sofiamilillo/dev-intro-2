b98b434f7498a491c32041df61e62bfb0a24d9bc
Emanuele <egalli64@gmail.com>
2023-11-24 15:10:21 +0100

    variable

---
+++ m4/s3_variables.py
@@ -0,0 +1,22 @@
+"""
+ Introduction to Software Development (Python)
+
+ https://github.com/egalli64/dev-intro
+
+ Variables
+"""
+
+# declaration
+x = None
+
+# initialization
+x = 1
+
+# definition
+y = 4
+
+# assignment
+y = 6
+
+# unnamed temporary variable
+print("x + y is", x + y);
