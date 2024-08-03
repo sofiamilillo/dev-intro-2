c1888b584648f7a254926fec5085aea9fb1d5efb
Emanuele <egalli64@gmail.com>
2024-08-03 16:30:04 +0200

    A couple of basic instructions

---
+++ src/main/java/M2S4a_Base.java
@@ -0,0 +1,21 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Some basic instructions
+ */
+
+void main() {
+    int a = 7;
+    System.out.println("a is " + a);
+
+    int b = 3;
+    System.out.println("b is " + b);
+
+    int sum = a + b;
+    System.out.println("a + b is " + sum);
+
+    int prod = a * b;
+    System.out.println("a * b is " + prod);
+}
