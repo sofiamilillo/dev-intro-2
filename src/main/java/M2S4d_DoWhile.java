a8c9c540c451dcc1d8f54f3c7ca03567c724656d
Emanuele <egalli64@gmail.com>
2024-08-03 17:08:33 +0200

    do while

---
+++ src/main/java/M2S4d_DoWhile.java
@@ -0,0 +1,16 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Loop do-while
+ */
+
+void main() {
+    int i = 0;
+
+    do {
+        System.out.println(i);
+        i++;
+    } while (i < 10);
+}
