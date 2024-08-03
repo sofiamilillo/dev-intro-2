a387f11bdea5a9912b7e4c657d97870c13a7ec58
Emanuele <egalli64@gmail.com>
2024-08-03 16:36:40 +0200

    while

---
+++ src/main/java/M2S4c_While.java
@@ -0,0 +1,16 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Loop while
+ */
+
+void main() {
+    int i = 0;
+
+    while (i < 10) {
+        System.out.println(i);
+        i += 1;
+    }
+}
