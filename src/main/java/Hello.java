054a1006f4f4566fbfd2fbc6ae8d76c2407f1fd4
Emanuele <egalli64@gmail.com>
2024-08-03 11:23:39 +0200

    Full fledged hello

---
+++ src/main/java/Hello.java
@@ -0,0 +1,14 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Hello Java
+ * Run it from CLI by: java Hello.java
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
