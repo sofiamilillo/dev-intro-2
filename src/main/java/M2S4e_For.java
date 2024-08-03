4346bea749b0c3cdc2d1ab3bfb36d683c62a829e
Emanuele <egalli64@gmail.com>
2024-08-03 17:09:49 +0200

    for loop

---
+++ src/main/java/M2S4e_For.java
@@ -0,0 +1,13 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Loop for
+ */
+
+void main() {
+    for (int i = 0; i < 10; i++) {
+        System.out.println(i);
+    }
+}
