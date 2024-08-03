929a3a594cb10454e4fea094ca6cb810c48cb757
Emanuele <egalli64@gmail.com>
2024-08-03 16:31:50 +0200

    if - else

---
+++ src/main/java/M2S4b_If.java
@@ -0,0 +1,45 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Decision by if-else
+ */
+
+void main() {
+    int a = 7;
+    System.out.println("a is " + a);
+
+    int b = 3;
+    System.out.println("b is " + b);
+
+    // if
+    if (a < b) {
+        System.out.println("a is less than b");
+    }
+
+    // if - else
+    if (a == b) {
+        System.out.println("a is equal to b");
+    } else {
+        System.out.println("a is not equal to b");
+    }
+
+    // if - else if
+    if (a > b) {
+        System.out.println("a is bigger than b");
+    } else if (b > a) {
+        System.out.println("b is bigger than a");
+    }
+
+    // if - else if - else
+    if (a > b) {
+        System.out.println("a is bigger than b");
+    } else if (b > a) {
+        System.out.println("b is bigger than a");
+    } else {
+        System.out.println("a is equal to b");
+    }
+
+    // ...
+}
