6163813c650f43ad34db8b1ed0e3be8b86838215
Emanuele <egalli64@gmail.com>
2024-08-03 17:11:15 +0200

    break

---
+++ src/main/java/M2S4f_Break.java
@@ -0,0 +1,19 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Interrupt a loop with break
+ */
+
+void main() {
+    int i = 0;
+
+    while (true) {
+        System.out.println(i);
+        i++;
+        if (i > 9) {
+            break;
+        }
+    }
+}
