f7776e812eb65f6e4d2ed908e9c3c9b1c7b3736c
Emanuele <egalli64@gmail.com>
2024-08-03 12:17:05 +0200

    boolean operators

---
+++ src/main/java/M1S2_Boole.java
@@ -0,0 +1,32 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Boolean variables and operators
+ */
+
+void main() {
+    // a boolean variable
+    boolean a = true;
+    System.out.println("a is " + a);
+
+    // another boolean variable
+    boolean b = false;
+    System.out.println("b is " + b);
+
+    // AND
+    boolean conjunction = a && b;
+    System.out.println("a and b is " + conjunction);
+
+    // OR
+    boolean inclusiveDisjunction = a || b;
+    System.out.println("a or b is " + inclusiveDisjunction);
+
+    // NOT
+    System.out.println("not a is " + !a);
+
+    // XOR
+    boolean exclusiveDisjunction = a ^ b;
+    System.out.println("a xor b is " + exclusiveDisjunction);
+}
