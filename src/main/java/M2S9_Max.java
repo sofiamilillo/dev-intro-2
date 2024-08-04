c10853677b31cde1fb3574707c444d26b87d0f15
Emanuele <egalli64@gmail.com>
2024-08-04 09:51:05 +0200

    From flow chart to code

---
+++ src/main/java/M2S9_Max.java
@@ -0,0 +1,22 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * From flow chart / pseudo-code to code: max value in an array
+ */
+
+void main() {
+    // indices:  0,  1,  2, 3,  4,  5
+    int[] a = { 12, 3, -10, 0, 50, 15 };
+
+    int max = a[0];
+
+    for (int i = 1; i < a.length; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+
+    System.out.println("The biggest value is: " + max);
+}
