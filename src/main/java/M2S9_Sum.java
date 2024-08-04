0cb44c10f3fce9ee8898dbeae2692d02e9a91e32
Emanuele <egalli64@gmail.com>
2024-08-04 09:54:19 +0200

    From flow chart to code

---
+++ src/main/java/M2S9_Sum.java
@@ -0,0 +1,18 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * From flow chart / pseudo-code to code: sum of values in an array
+ */
+
+void main() {
+    int[] data = { 1, 42, 13, 3, 45, 2 };
+
+    int result = 0;
+    for (int i = 0; i < data.length; i++) {
+        result += data[i];
+    }
+
+    System.out.println("Sum of array is " + result);
+}
