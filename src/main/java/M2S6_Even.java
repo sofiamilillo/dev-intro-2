ddec005a341ddeaa86db0b10391c19122c242e3d
Emanuele <egalli64@gmail.com>
2024-08-04 09:26:38 +0200

    From flow chart to code

---
+++ src/main/java/M2S6_Even.java
@@ -0,0 +1,36 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * From a flow chart to code: print only the even numbers in an array
+ */
+
+// an array of integers
+int[] data = { 1, 42, 13, 3, 45, 2 };
+
+/**
+ * This code almost doesn't look Java The idea is following the related
+ * flowchart blindly. We'll see how to rewrite it in a more idiomatic way
+ */
+void main() {
+    if (data.length == 0) {
+        System.out.println("Done");
+    } else {
+        int i = 0;
+
+        while (true) {
+            // check if the current value in the array is even
+            if (data[i] % 2 == 0) {
+                System.out.println(data[i]);
+            }
+
+            // check if it is the last element
+            if (i == data.length - 1) {
+                break;
+            }
+            i = i + 1;
+        }
+        System.out.println("Done");
+    }
+}
