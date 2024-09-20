9506c3f7487e191ac7707a26de7eb6fd82e05e3c
Emanuele <egalli64@gmail.com>
2024-09-20 08:32:11 +0200

    readability

--- src/main/java/M2S9_Palindrome.java
+++ src/main/java/M2S9_Palindrome.java
@@ -10,14 +10,14 @@ void main() {
     // indices: 0123
     String s = "abba";
 
-    boolean isPal = true;
+    boolean isPalindrome = true;
 
     for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
         if (s.charAt(i) != s.charAt(j)) {
-            isPal = false;
+            isPalindrome = false;
             break;
         }
     }
 
-    System.out.println(s + " is a palindrome? " + isPal);
+    System.out.println(s + " is a palindrome? " + isPalindrome);
 }
