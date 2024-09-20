a28d15946a855b6ba2c42f7d0d45ad9af3f40037
Emanuele <egalli64@gmail.com>
2024-09-20 08:35:11 +0200

    simplify

--- src/main/java/M5S4_Bubble.java
+++ src/main/java/M5S4_Bubble.java
@@ -1,3 +1,4 @@
+
 /*
  * Introduction to Software Development (Java)
  * 
@@ -5,14 +6,16 @@
  *
  * Bubble sort - most basic implementation
  */
+import java.util.Arrays;
 
 void main() {
     int[] data = { 4, 5, 1, 8, 2 };
-    System.out.println("The array: " + java.util.Arrays.toString(data));
+    System.out.println("The array: " + Arrays.toString(data));
 
     // repeat n-1 times
     for (int i = 0; i < data.length - 1; i++) {
         // compare the neighbors, and swap if required
+
         for (int j = 0; j < data.length - 1; j++) {
             if (data[j] > data[j + 1]) {
                 int temp = data[j];
@@ -24,5 +27,5 @@ void main() {
         }
     }
 
-    System.out.println("The sorted array: " + java.util.Arrays.toString(data));
+    System.out.println("The sorted array: " + Arrays.toString(data));
 }
