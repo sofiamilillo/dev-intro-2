1bda2e9a63c116d44b1d583f5d957ae0e83abd65
Emanuele <egalli64@gmail.com>
2024-09-20 08:38:23 +0200

    refactored to simplified Java

---
+++ src/main/java/M5S4_Selection.java
@@ -0,0 +1,32 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Selection sort - most basic implementation
+ */
+import java.util.Arrays;
+
+void main() {
+    int[] data = { 4, 5, 1, 8, 2 };
+    System.out.println("The array: " + Arrays.toString(data));
+
+    // for each position (but the last one)
+    for (int i = 0; i < data.length - 1; i++) {
+        // find the position of the smallest element to the right
+        int pos = i;
+        for (int j = i + 1; j < data.length; j++) {
+            if (data[j] < data[pos]) {
+                pos = j;
+            }
+        }
+
+        // swap current and minimal elements
+        System.out.printf("Swap between %d and %d%n", data[i], data[pos]);
+        int temp = data[i];
+        data[i] = data[pos];
+        data[pos] = temp;
+    }
+
+    System.out.println("The sorted array: " + Arrays.toString(data));
+}
