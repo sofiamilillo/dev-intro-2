dacf2d1f3e749e5c40d23f91614f518b927ea234
Emanuele <egalli64@gmail.com>
2024-09-21 19:03:24 +0200

    readability

--- src/main/java/M5S4_Selection.java
+++ src/main/java/M5S4_Selection.java
@@ -8,25 +8,25 @@
 import java.util.Arrays;
 
 void main() {
-    int[] data = { 4, 5, 1, 8, 2 };
-    System.out.println("The array: " + Arrays.toString(data));
+    int[] values = { 4, 5, 1, 8, 2 };
+    System.out.println("The array: " + Arrays.toString(values));
 
     // for each position (but the last one)
-    for (int i = 0; i < data.length - 1; i++) {
+    for (int i = 0; i < values.length - 1; i++) {
         // find the position of the smallest element to the right
-        int pos = i;
-        for (int j = i + 1; j < data.length; j++) {
-            if (data[j] < data[pos]) {
-                pos = j;
+        int posMin = i;
+        for (int j = i + 1; j < values.length; j++) {
+            if (values[j] < values[posMin]) {
+                posMin = j;
             }
         }
 
         // swap current and minimal elements
-        System.out.printf("Swap between %d and %d%n", data[i], data[pos]);
-        int temp = data[i];
-        data[i] = data[pos];
-        data[pos] = temp;
+        System.out.printf("Swap between position %d (%d) and %d (%d)\n", i, values[i], posMin, values[posMin]);
+        int temp = values[i];
+        values[i] = values[posMin];
+        values[posMin] = temp;
     }
 
-    System.out.println("The sorted array: " + Arrays.toString(data));
+    System.out.println("The sorted array: " + Arrays.toString(values));
 }
