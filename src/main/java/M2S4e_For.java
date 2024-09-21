0b5d39f42e6a6312033ded505800de92691e459c
Emanuele <egalli64@gmail.com>
2024-09-21 19:13:28 +0200

    clarify

--- src/main/java/M2S4e_For.java
+++ src/main/java/M2S4e_For.java
@@ -7,7 +7,7 @@
  */
 
 void main() {
-    for (int i = 0; i < 10; i++) {
-        System.out.println(i);
+    for (int value = 0; value < 10; value++) {
+        System.out.println(value);
     }
 }
