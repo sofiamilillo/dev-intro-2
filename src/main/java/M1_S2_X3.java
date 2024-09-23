11e160952545a79fa8e5d578011a79b030eb58e2
Emanuele <egalli64@gmail.com>
2024-09-23 18:13:26 +0200

    exercise

---
+++ src/main/java/M1_S2_X3.java
@@ -0,0 +1,18 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Exercises on boolean variables and operators
+ */
+
+void main() {
+    int left = -42;
+    int right = -18;
+    System.out.println("left is " + left);
+    System.out.println("right is " + right);
+
+    // TODO: set positive to true if left, right or both are greater than zero
+    boolean positive = true;
+    System.out.println("left, right, or both are positive: " + positive);
+}
