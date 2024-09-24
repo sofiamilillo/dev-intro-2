4a13e0bd5c5a6a00a70f4f1a46866856f56be86f
Emanuele <egalli64@gmail.com>
2024-09-24 08:34:23 +0200

    exercise

---
+++ src/main/java/M1_S2_X4.java
@@ -0,0 +1,20 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Exercises on boolean variables and operators
+ */
+
+void main() {
+    int target = 42;
+    System.out.println("target is " + target);
+
+    // TODO: set negative to true if target is less than zero
+    boolean negative = true;
+    System.out.println("target is negative: " + negative);
+
+    // TODO: set notNegative to true if negative is false
+    boolean notNegative = false;
+    System.out.println("target is positive: " + notNegative);
+}
