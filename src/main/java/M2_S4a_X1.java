e7ab386dc7dfb8d4eee4a6a478891e45dd347799
Emanuele <egalli64@gmail.com>
2024-09-24 08:40:35 +0200

    exercise

---
+++ src/main/java/M2_S4a_X1.java
@@ -0,0 +1,19 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Exercise on division
+ */
+
+void main() {
+    int a = 42;
+    System.out.println("a is " + a);
+
+    int b = 6;
+    System.out.println("b is " + b);
+
+    // TODO: assign to c the result of the division of a by b
+    int c = 0;
+    System.out.println("a divided by b is " + c);
+}
